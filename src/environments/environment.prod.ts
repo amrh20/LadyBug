c55be81f43c5bbd965b6048d1a365b29edb1c8b6
amrh20 <amrhamdy058@gmail.com>
2022-11-29 16:39:43 +0200

    edit file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: ''
+  baseUrl: "http://54.160.5.149/ladybug2/api/v1/",
 };
--- src/environments/environment.stg.ts
+++ src/environments/environment.stg.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    baseUrl: ''
+  production: true,
+  baseUrl: "http://54.160.5.149/ladybug2/api/v1/",
 };
