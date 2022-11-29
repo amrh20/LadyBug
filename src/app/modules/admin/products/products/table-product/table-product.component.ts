65cb80b7d5c210c6295a5c3dae2ef97fff418e0a
amrh20 <amrhamdy058@gmail.com>
2022-11-29 17:23:02 +0200

    edit file

--- src/app/modules/admin/products/products/table-product/table-product.component.ts
+++ src/app/modules/admin/products/products/table-product/table-product.component.ts
@@ -1,29 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { SettingService } from 'src/app/core/services/setting.service';
-import { CrudRequestsService } from '../../../../../core/services/crud-requests.service';
-import Swal from 'sweetalert2';
+import { Component, OnInit } from "@angular/core";
+import { SettingService } from "src/app/core/services/setting.service";
+import { CrudRequestsService } from "../../../../../core/services/crud-requests.service";
+import Swal from "sweetalert2";
 
 @Component({
-  selector: 'app-table-product',
-  templateUrl: './table-product.component.html',
-  styleUrls: ['./table-product.component.scss']
+  selector: "app-table-product",
+  templateUrl: "./table-product.component.html",
+  styleUrls: ["./table-product.component.scss"],
 })
 export class TableProductComponent implements OnInit {
-  DataTable:any=[];
+  DataTable: any = [];
   constructor(
     private _CrudRequestsService: CrudRequestsService,
     private _SettingService: SettingService
   ) {}
   ngOnInit(): void {
     this.getUsers();
- 
   }
   getUsers = () => {
-    this._CrudRequestsService.get("reports").subscribe((data: any) => {
-      this.DataTable = data.data.all;
+    this._CrudRequestsService.get("products").subscribe((data: any) => {
+      this.DataTable = data.data.data;
     });
   };
-  
+
   deleteItem = (id: any) => {
     Swal.fire({
       text: "   هل أنت متاكد من الحذف  ؟",
@@ -37,16 +36,16 @@ export class TableProductComponent implements OnInit {
       cancelButtonAriaLabel: "التراجع",
     }).then((val: any) => {
       if (val.isConfirmed) {
-        
-        this._CrudRequestsService.delete("post_types/"+id).subscribe( (res: any) => {
-          this._SettingService.successHot(res.message);
-          this.getUsers();
-        },
-        (err) => {
-          this._SettingService.errorHot(err.message);
-        });
+        this._CrudRequestsService.delete("post_types/" + id).subscribe(
+          (res: any) => {
+            this._SettingService.successHot(res.message);
+            this.getUsers();
+          },
+          (err) => {
+            this._SettingService.errorHot(err.message);
+          }
+        );
       }
     });
   };
-
 }
