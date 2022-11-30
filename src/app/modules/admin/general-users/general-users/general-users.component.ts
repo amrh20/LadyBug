b06b951b55eca8c7f11cf7519862cb9e972b64fc
amrh20 <amrhamdy058@gmail.com>
2022-11-30 09:54:27 +0200

    search

--- src/app/modules/admin/general-users/general-users/general-users.component.ts
+++ src/app/modules/admin/general-users/general-users/general-users.component.ts
@@ -1,61 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-import { CrudRequestsService } from '../../../../core/services/crud-requests.service';
-import { SettingService } from '../../../../core/services/setting.service';
-import Swal from 'sweetalert2';
+import { Component, OnInit } from "@angular/core";
+import { CrudRequestsService } from "../../../../core/services/crud-requests.service";
+import { SettingService } from "../../../../core/services/setting.service";
+import Swal from "sweetalert2";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-general-users',
-  templateUrl: './general-users.component.html',
-  styleUrls: ['./general-users.component.scss']
+  selector: "app-general-users",
+  templateUrl: "./general-users.component.html",
+  styleUrls: ["./general-users.component.scss"],
 })
 export class GeneralUsersComponent implements OnInit {
-
-  constructor(private _CrudRequestsService:CrudRequestsService,private _SettingService:SettingService) { }
+  filterForm = new FormGroup({
+    name: new FormControl(""),
+    email: new FormControl(""),
+    mobile: new FormControl(""),
+  });
+  constructor(
+    private _CrudRequestsService: CrudRequestsService,
+    private _SettingService: SettingService
+  ) {}
   ngOnInit(): void {
-    this.getUsers()
+    this.getUsers();
   }
-  DataTable:any=[];
-  getUsers =()=>{
-    this._CrudRequestsService.get('generic_users').subscribe((data:any)=>{
-    this.DataTable=data.data.all;
-    })
-  }
+  DataTable: any = [];
+  getUsers = () => {
+    this._CrudRequestsService.get("generic_users").subscribe((data: any) => {
+      this.DataTable = data.data.all;
+    });
+  };
 
-  blockItem =(id:any,$event:any)=>{
-    console.log(id)
-    console.log($event.checked)
-    this._CrudRequestsService.get(`users/toggle_activate/${id}`).subscribe((data:any)=>{
-    if(data.success){
-      this._SettingService.successHot(data.message)
-    }else{
-      this._SettingService.errorHot(data.message)
+  blockItem = (id: any, $event: any) => {
+    console.log(id);
+    console.log($event.checked);
+    this._CrudRequestsService.get(`users/toggle_activate/${id}`).subscribe(
+      (data: any) => {
+        if (data.success) {
+          this._SettingService.successHot(data.message);
+        } else {
+          this._SettingService.errorHot(data.message);
+        }
+      },
+      (err: any) => {
+        this._SettingService.errorHot(err.message);
+      }
+    );
+  };
 
-    }
-    },(err:any)=>{
-      this._SettingService.errorHot(err.message)
-
-    })
-  }
-
-  deleteItem =(id:any)=>{
+  deleteItem = (id: any) => {
     Swal.fire({
-      text: '   هل أنت متاكد من الحذف  ؟',
+      text: "   هل أنت متاكد من الحذف  ؟",
       allowOutsideClick: true,
       // iconHtml:"<img src='../../../../assets/images/delete-alert.svg'/>",
       showCloseButton: true,
       showCancelButton: true,
       focusConfirm: false,
-      confirmButtonText:
-      ' تأكيد الحذف',
-      confirmButtonAriaLabel:  ' تأكيد الحذف',
-      cancelButtonText:
-      'التراجع',
-    cancelButtonAriaLabel: 'التراجع'
-      }).then((val: any)=>{
-        if(val.isConfirmed) {
-   
-        }
-       
-      })
+      confirmButtonText: " تأكيد الحذف",
+      confirmButtonAriaLabel: " تأكيد الحذف",
+      cancelButtonText: "التراجع",
+      cancelButtonAriaLabel: "التراجع",
+    }).then((val: any) => {
+      if (val.isConfirmed) {
+      }
+    });
+  };
+
+  search() {
+    let name = this.filterForm.get("name")?.value;
+    console.log(name);
+    let email = this.filterForm.get("email")?.value;
+    let mobile = this.filterForm.get("mobile")?.value;
+    this._CrudRequestsService
+      .get(`generic_users?name=${name}&email=${email}&mobile=${mobile}`)
+      .subscribe((data: any) => {
+        this.DataTable = data.data.all;
+      });
   }
 }
