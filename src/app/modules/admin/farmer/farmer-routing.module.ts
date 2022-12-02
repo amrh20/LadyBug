e4a75575f24f8426a629729db7803fc6a2cc356d
amrh20 <amrhamdy058@gmail.com>
2022-12-02 20:20:05 +0200

    solve

--- src/app/modules/admin/farmer/farmer-routing.module.ts
+++ src/app/modules/admin/farmer/farmer-routing.module.ts
@@ -61,51 +61,59 @@ const routes: Routes = [
             (m) => m.AcidityTypesModule
           ),
       },
+
+      {
+        path: "FarmingWays",
+        loadChildren: () =>
+          import("./farming-ways/farming-ways.module").then(
+            (m) => m.FarmingWaysModule
+          ),
+      },
+      {
+        path: "FarmingMethods",
+        loadChildren: () =>
+          import("./farming-methods/farming-methods.module").then(
+            (m) => m.FarmingMethodsModule
+          ),
+      },
+      {
+        path: "IrrigationMethods",
+        loadChildren: () =>
+          import("./irrigation-methods/irrigation-methods.module").then(
+            (m) => m.IrrigationMethodsModule
+          ),
+      },
+      {
+        path: "AnimalBreedingPurposes",
+        loadChildren: () =>
+          import("./animals-purposes/animals-purposes.module").then(
+            (m) => m.AnimalsPurposesModule
+          ),
+      },
+      //
+      {
+        path: "SoilTypes",
+        loadChildren: () =>
+          import("./soil-types/soil-types.module").then(
+            (m) => m.SoilTypesModule
+          ),
+      },
+      {
+        path: "FeedTypes",
+        loadChildren: () =>
+          import("./feed-types/feed-types.module").then(
+            (m) => m.FeedTypesModule
+          ),
+      },
+      {
+        path: "TypesSalts",
+        loadChildren: () =>
+          import("./types-salts/types-salts.module").then(
+            (m) => m.TypesSaltsModule
+          ),
+      },
     ],
   },
-          import("./type-of-cultivar/type-of-cultivar.module").then((m) => m.TypeOfCultivarModule),
-      },
-      {
-        path:'FarmingWays',
-        loadChildren: () =>
-          import("./farming-ways/farming-ways.module").then((m) => m.FarmingWaysModule),
-      },
-      {
-        path:'FarmingMethods',
-        loadChildren: () =>
-          import("./farming-methods/farming-methods.module").then((m) => m.FarmingMethodsModule),
-        
-      },
-      {
-        path:'IrrigationMethods',
-        loadChildren: () =>
-        import("./irrigation-methods/irrigation-methods.module").then((m) => m.IrrigationMethodsModule),
-      
-      },
-      {
-        path:'AnimalBreedingPurposes',
-        loadChildren: () =>
-        import("./animals-purposes/animals-purposes.module").then((m) => m.AnimalsPurposesModule),
-      
-      },
-      //  
-      {
-        path:'SoilTypes',
-        loadChildren: () =>
-        import("./soil-types/soil-types.module").then((m) => m.SoilTypesModule),
-      },
-      {
-        path:'FeedTypes',
-        loadChildren: () =>
-        import("./feed-types/feed-types.module").then((m) => m.FeedTypesModule),
-      },
-      {
-        path:'TypesSalts',
-        loadChildren: () =>
-        import("./types-salts/types-salts.module").then((m) => m.TypesSaltsModule),
-      }
-    ]
-  }
 ];
 
 @NgModule({
