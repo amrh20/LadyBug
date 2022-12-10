4cdc7aa254744c10b432a7bcf2478b047a83ea68
Kareem Elsayed <kelsayed.c@saip.gov.sa>
2022-12-10 16:46:40 +0200

    insecticides

--- src/app/modules/admin/farmer/farmes-type/farmes-type-routing.module.ts
+++ src/app/modules/admin/farmer/farmes-type/farmes-type-routing.module.ts
@@ -7,6 +7,9 @@ import { FarmedTypeNutValsTableComponent } from './farmes-type-table/views/farme
 import { FarmedTypeExtrasTableComponent } from './farmes-type-table/views/farmed_type_extras/farmed-type-extras-table/farmed-type-extras-table.component';
 import { TaxonomiesTableComponent } from './farmes-type-table/views/taxonomies/taxonomies-table/taxonomies-table.component';
 import { TaxonomiesCreateComponent } from './farmes-type-table/views/taxonomies/taxonomies-create/taxonomies-create.component';
+import { TaxonomiesDetailsComponent } from './farmes-type-table/views/taxonomies/taxonomies-details/taxonomies-details.component';
+import { FarmedTypeNutValsCreateComponent } from './farmes-type-table/views/farmed_type_nut_vals/farmed-type-nut-vals-create/farmed-type-nut-vals-create.component';
+import { FarmedTypeNutValsDetalisComponent } from './farmes-type-table/views/farmed_type_nut_vals/farmed-type-nut-vals-detalis/farmed-type-nut-vals-detalis.component';
 
 const routes: Routes = [{
   path:'',
@@ -29,10 +32,16 @@ const routes: Routes = [{
   component:TaxonomiesTableComponent
 },
 {
-  // 
   path:'taxonomies/Create/:id',
   component:TaxonomiesCreateComponent
-
+},
+{
+  path:'taxonomies/edit/:id',
+  component:TaxonomiesCreateComponent
+},
+{
+  path:'taxonomies/details/:id',
+  component:TaxonomiesDetailsComponent
 }
 ,{
   path:'farmed_type_extras/:id',
@@ -42,6 +51,17 @@ const routes: Routes = [{
 ,{
   path:'farmed_type_nut_vals/:id',
   component:FarmedTypeNutValsTableComponent
+},{
+  path:'farmed_type_nut_vals/Create/:id',
+  component:FarmedTypeNutValsCreateComponent
+},
+{
+  path:'farmed_type_nut_vals/edit/:id',
+  component:FarmedTypeNutValsCreateComponent
+},
+{
+  path:'farmed_type_nut_vals/details/:id',
+  component:FarmedTypeNutValsDetalisComponent
 }
 ];
 
--- src/app/modules/admin/farmer/farmes-type/farmes-type-table/views/farmed_type_nut_vals/farmed-type-nut-vals-table/farmed-type-nut-vals-table.component.ts
+++ src/app/modules/admin/farmer/farmes-type/farmes-type-table/views/farmed_type_nut_vals/farmed-type-nut-vals-table/farmed-type-nut-vals-table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CrudRequestsService } from '../../../../../../../../core/services/crud-requests.service';
 import { SettingService } from '../../../../../../../../core/services/setting.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-farmed-type-nut-vals-table',
@@ -10,6 +11,34 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class FarmedTypeNutValsTableComponent implements OnInit {
 
+  deleteItem = (id: any) => {
+    Swal.fire({
+      text: "   هل أنت متاكد من الحذف  ؟",
+      allowOutsideClick: true,
+      showCloseButton: true,
+      showCancelButton: true,
+      focusConfirm: false,
+      confirmButtonText: "تأكيد",
+      confirmButtonAriaLabel: " تأكيد الحجر",
+      cancelButtonText: "التراجع",
+      cancelButtonAriaLabel: "التراجع",
+    }).then((val: any) => {
+      if (val.isConfirmed) {
+        this._CrudRequestsService.delete("farmed_type_nut_vals/" + id).subscribe(
+          (res: any) => {
+            this._SettingService.successHot(res.message);
+            this.getUser(this.ids);
+          },
+          (err) => {
+            this._SettingService.errorHot(err.message);
+          }
+        );
+      }
+    });
+  };
+  ids:any=1;
+
+  DataTable:any=[];
   constructor(
     private _CrudRequestsService: CrudRequestsService,
     private _SettingService: SettingService,
@@ -21,12 +50,17 @@ export class FarmedTypeNutValsTableComponent implements OnInit {
   ngOnInit(): void {
     this._activeRoute.params.subscribe((params) => {
       this.getUser(params["id"]);
+      this.ids=params["id"];
     });
   }
-  detailsData: any = {};
+  success:any;
+  detailsData: any;
+  res:any;
   getUser = (id: any) => {
-    this._CrudRequestsService.get("farmed_type_nut_vals/by_ft_id/" + id).subscribe((data: any) => {
-      this.detailsData = data.data;
+    this.success=false;
+    this._CrudRequestsService.get("farmed_type_nut_vals/by_ft_id/"+id).subscribe((data: any) => {
+      this.res=data.data;
+      this.success=data.data.code;
     });
   };
 }
