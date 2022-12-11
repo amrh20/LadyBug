0f3f8f1f8a98e3920f16ce72556dd3d14d88650e
amrh20 <amrhamdy058@gmail.com>
2022-12-11 20:23:46 +0200

    farmed types module

--- src/app/modules/admin/farmer/farmes-type/farmes-type.module.ts
+++ src/app/modules/admin/farmer/farmes-type/farmes-type.module.ts
@@ -1,30 +1,30 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { FarmesTypeRoutingModule } from './farmes-type-routing.module';
-import { FarmesTypeTableComponent } from './farmes-type-table/farmes-type-table.component';
-import { FarmesTypeCreateComponent } from './farmes-type-create/farmes-type-create.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { TranslateModule } from '@ngx-translate/core';
-import { SharedModule } from '../../../../shared/shared.module';
-import { MaterialModule } from '../../../../core/material-module.module';
-import { FarmesTypeDetailsComponent } from './farmes-type-details/farmes-type-details.component';
-import { FarmedTypeExtrasTableComponent } from './farmes-type-table/views/farmed_type_extras/farmed-type-extras-table/farmed-type-extras-table.component';
-import { FarmedTypeExtrasCreateComponent } from './farmes-type-table/views/farmed_type_extras/farmed-type-extras-create/farmed-type-extras-create.component';
-import { FarmedTypeExtrasDetasilComponent } from './farmes-type-table/views/farmed_type_extras/farmed-type-extras-detasil/farmed-type-extras-detasil.component';
-import { TaxonomiesCreateComponent } from './farmes-type-table/views/taxonomies/taxonomies-create/taxonomies-create.component';
-import { TaxonomiesTableComponent } from './farmes-type-table/views/taxonomies/taxonomies-table/taxonomies-table.component';
-import { TaxonomiesDetailsComponent } from './farmes-type-table/views/taxonomies/taxonomies-details/taxonomies-details.component';
-import { FarmedTypeNutValsTableComponent } from './farmes-type-table/views/farmed_type_nut_vals/farmed-type-nut-vals-table/farmed-type-nut-vals-table.component';
-import { FarmedTypeNutValsCreateComponent } from './farmes-type-table/views/farmed_type_nut_vals/farmed-type-nut-vals-create/farmed-type-nut-vals-create.component';
-import { FarmedTypeNutValsDetalisComponent } from './farmes-type-table/views/farmed_type_nut_vals/farmed-type-nut-vals-detalis/farmed-type-nut-vals-detalis.component';
-import { MarketingDatasCreateComponent } from './farmes-type-table/views/marketing_datas/marketing-datas-create/marketing-datas-create.component';
-import { MarketingDatasTableComponent } from './farmes-type-table/views/marketing_datas/marketing-datas-table/marketing-datas-table.component';
-import { MarketingDatasDetailsComponent } from './farmes-type-table/views/marketing_datas/marketing-datas-details/marketing-datas-details.component';
-import { FertilizationNeedsCreateComponent } from './farmes-type-table/views/fertilization_needs/fertilization-needs-create/fertilization-needs-create.component';
-import { FertilizationNeedsDetailsComponent } from './farmes-type-table/views/fertilization_needs/fertilization-needs-details/fertilization-needs-details.component';
-import { FertilizationNeedsTableComponent } from './farmes-type-table/views/fertilization_needs/fertilization-needs-table/fertilization-needs-table.component';
-
+import { FarmesTypeRoutingModule } from "./farmes-type-routing.module";
+import { FarmesTypeTableComponent } from "./farmes-type-table/farmes-type-table.component";
+import { FarmesTypeCreateComponent } from "./farmes-type-create/farmes-type-create.component";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { TranslateModule } from "@ngx-translate/core";
+import { SharedModule } from "../../../../shared/shared.module";
+import { MaterialModule } from "../../../../core/material-module.module";
+import { FarmesTypeDetailsComponent } from "./farmes-type-details/farmes-type-details.component";
+import { FarmedTypeExtrasTableComponent } from "./farmes-type-table/views/farmed_type_extras/farmed-type-extras-table/farmed-type-extras-table.component";
+import { FarmedTypeExtrasCreateComponent } from "./farmes-type-table/views/farmed_type_extras/farmed-type-extras-create/farmed-type-extras-create.component";
+import { FarmedTypeExtrasDetasilComponent } from "./farmes-type-table/views/farmed_type_extras/farmed-type-extras-detasil/farmed-type-extras-detasil.component";
+import { TaxonomiesCreateComponent } from "./farmes-type-table/views/taxonomies/taxonomies-create/taxonomies-create.component";
+import { TaxonomiesTableComponent } from "./farmes-type-table/views/taxonomies/taxonomies-table/taxonomies-table.component";
+import { TaxonomiesDetailsComponent } from "./farmes-type-table/views/taxonomies/taxonomies-details/taxonomies-details.component";
+import { FarmedTypeNutValsTableComponent } from "./farmes-type-table/views/farmed_type_nut_vals/farmed-type-nut-vals-table/farmed-type-nut-vals-table.component";
+import { FarmedTypeNutValsCreateComponent } from "./farmes-type-table/views/farmed_type_nut_vals/farmed-type-nut-vals-create/farmed-type-nut-vals-create.component";
+import { FarmedTypeNutValsDetalisComponent } from "./farmes-type-table/views/farmed_type_nut_vals/farmed-type-nut-vals-detalis/farmed-type-nut-vals-detalis.component";
+import { MarketingDatasCreateComponent } from "./farmes-type-table/views/marketing_datas/marketing-datas-create/marketing-datas-create.component";
+import { MarketingDatasTableComponent } from "./farmes-type-table/views/marketing_datas/marketing-datas-table/marketing-datas-table.component";
+import { MarketingDatasDetailsComponent } from "./farmes-type-table/views/marketing_datas/marketing-datas-details/marketing-datas-details.component";
+import { FertilizationNeedsCreateComponent } from "./farmes-type-table/views/fertilization_needs/fertilization-needs-create/fertilization-needs-create.component";
+import { FertilizationNeedsDetailsComponent } from "./farmes-type-table/views/fertilization_needs/fertilization-needs-details/fertilization-needs-details.component";
+import { FertilizationNeedsTableComponent } from "./farmes-type-table/views/fertilization_needs/fertilization-needs-table/fertilization-needs-table.component";
+import { NgxMultipleSelectModule } from "ngx-multiple-select";
 
 @NgModule({
   declarations: [
@@ -45,7 +45,7 @@ import { FertilizationNeedsTableComponent } from './farmes-type-table/views/fert
     MarketingDatasDetailsComponent,
     FertilizationNeedsCreateComponent,
     FertilizationNeedsDetailsComponent,
-    FertilizationNeedsTableComponent
+    FertilizationNeedsTableComponent,
   ],
   imports: [
     CommonModule,
@@ -55,6 +55,7 @@ import { FertilizationNeedsTableComponent } from './farmes-type-table/views/fert
     TranslateModule,
     FormsModule,
     ReactiveFormsModule,
-  ]
+    NgxMultipleSelectModule,
+  ],
 })
-export class FarmesTypeModule { }
+export class FarmesTypeModule {}
