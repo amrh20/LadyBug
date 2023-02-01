052dba11d1465798193c8e96a565733fdf34a6a4
amr <hamdy@innosoft.me>
2023-02-01 15:21:02 +0200

    edits

--- src/app/modules/admin/diseases/diseases-routing.module.ts
+++ src/app/modules/admin/diseases/diseases-routing.module.ts
@@ -1,46 +1,53 @@
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from "@angular/router";
 
-import { DiseaseRegistrationsTableComponent } from './pages/disease_registrations/disease-registrations-table/disease-registrations-table.component';
-import { DiseaseRegistrationsViewComponent } from './pages/disease_registrations/disease-registrations-view/disease-registrations-view.component';
-import { DiseasesCreateComponent } from './pages/diseases/diseases-create/diseases-create.component';
-import { DiseasesDetailsComponent } from './pages/diseases/diseases-details/diseases-details.component';
-import { DiseasesTableComponent } from './pages/diseases/diseases-table/diseases-table.component';
-import { IndexDiseasesComponent } from './index-diseases/index-diseases.component';
-import { InfectionRatesCreateComponent } from './pages/infection_rates/infection-rates-create/infection-rates-create.component';
-import { InfectionRatesTableComponent } from './pages/infection_rates/infection-rates-table/infection-rates-table.component';
-import { NgModule } from '@angular/core';
+import { DiseaseRegistrationsTableComponent } from "./pages/disease_registrations/disease-registrations-table/disease-registrations-table.component";
+import { DiseaseRegistrationsViewComponent } from "./pages/disease_registrations/disease-registrations-view/disease-registrations-view.component";
+import { DiseasesCreateComponent } from "./pages/diseases/diseases-create/diseases-create.component";
+import { DiseasesDetailsComponent } from "./pages/diseases/diseases-details/diseases-details.component";
+import { DiseasesTableComponent } from "./pages/diseases/diseases-table/diseases-table.component";
+import { IndexDiseasesComponent } from "./index-diseases/index-diseases.component";
+import { InfectionRatesCreateComponent } from "./pages/infection_rates/infection-rates-create/infection-rates-create.component";
+import { InfectionRatesTableComponent } from "./pages/infection_rates/infection-rates-table/infection-rates-table.component";
+import { NgModule } from "@angular/core";
 
 const routes: Routes = [
   {
-    path:'',
-    component:IndexDiseasesComponent,
-    children:[
+    path: "",
+    component: IndexDiseasesComponent,
+    children: [
       {
-        path:'',
-        redirectTo:'/indexDiseases',
-        pathMatch:'full'
+        path: "",
+        redirectTo: "/indexDiseases",
+        pathMatch: "full",
       },
-      { path: 'indexDiseases', component: DiseasesTableComponent },
-      { path: 'edit-Diseases/:id', component: DiseasesCreateComponent },
-      { path: 'create-Diseases', component:  DiseasesCreateComponent},
-      { path: 'Diseases/:id',component: DiseasesDetailsComponent },
-      { path: 'DiseaseRegistrationsView/:id',component: DiseaseRegistrationsViewComponent },
-
-      
-      { path: 'InfectionRates', component: InfectionRatesTableComponent },
-      { path: 'edit-InfectionRates/:id', component: InfectionRatesCreateComponent },
-      { path: 'create-InfectionRates', component:  InfectionRatesCreateComponent},
+      { path: "indexDiseases", component: DiseasesTableComponent },
+      { path: "edit-Diseases/:id", component: DiseasesCreateComponent },
+      { path: "create-Diseases", component: DiseasesCreateComponent },
+      { path: "diseases-details/:id", component: DiseasesDetailsComponent },
       {
-        path:'diseaseRegistrations',
-        component:DiseaseRegistrationsTableComponent
-      }
-    
-    ]
-  }
+        path: "diseaseRegistrationsView/:id",
+        component: DiseaseRegistrationsViewComponent,
+      },
+
+      { path: "InfectionRates", component: InfectionRatesTableComponent },
+      {
+        path: "edit-InfectionRates/:id",
+        component: InfectionRatesCreateComponent,
+      },
+      {
+        path: "create-InfectionRates",
+        component: InfectionRatesCreateComponent,
+      },
+      {
+        path: "diseaseRegistrations",
+        component: DiseaseRegistrationsTableComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DiseasesRoutingModule { }
+export class DiseasesRoutingModule {}
--- src/app/modules/admin/diseases/pages/diseases/diseases-details/diseases-details.component.ts
+++ src/app/modules/admin/diseases/pages/diseases/diseases-details/diseases-details.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { CrudRequestsService } from "src/app/core/services/crud-requests.service";
+import { SettingService } from "src/app/core/services/setting.service";
 
 @Component({
-  selector: 'app-diseases-details',
-  templateUrl: './diseases-details.component.html',
-  styleUrls: ['./diseases-details.component.scss']
+  selector: "app-diseases-details",
+  templateUrl: "./diseases-details.component.html",
+  styleUrls: ["./diseases-details.component.scss"],
 })
 export class DiseasesDetailsComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(
+    private _CrudRequestsService: CrudRequestsService,
+    private _SettingService: SettingService,
+    private _crud: CrudRequestsService,
+    private _setting: SettingService,
+    private route: Router,
+    private _activeRoute: ActivatedRoute
+  ) {}
   ngOnInit(): void {
+    this._activeRoute.params.subscribe((params) => {
+      this.getUser(params["id"]);
+    });
   }
-
+  detailsData: any = {};
+  getUser = (id: any) => {
+    this._CrudRequestsService
+      .get("admin/diseases/" + id)
+      .subscribe((data: any) => {
+        this.detailsData = data.data;
+      });
+  };
 }
