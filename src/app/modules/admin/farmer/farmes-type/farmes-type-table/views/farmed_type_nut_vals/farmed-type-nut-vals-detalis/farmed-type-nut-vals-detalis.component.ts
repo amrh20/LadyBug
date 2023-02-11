ec1171b2d783fb4a39f031aa348b6511995a4d2f
amr <hamdy@innosoft.me>
2023-02-11 20:30:11 +0200

    solve

--- src/app/modules/admin/farmer/farmes-type/farmes-type-table/views/farmed_type_nut_vals/farmed-type-nut-vals-detalis/farmed-type-nut-vals-detalis.component.ts
+++ src/app/modules/admin/farmer/farmes-type/farmes-type-table/views/farmed_type_nut_vals/farmed-type-nut-vals-detalis/farmed-type-nut-vals-detalis.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { CrudRequestsService } from '../../../../../../../../core/services/crud-requests.service';
-import { SettingService } from '../../../../../../../../core/services/setting.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { CrudRequestsService } from "../../../../../../../../core/services/crud-requests.service";
+import { SettingService } from "../../../../../../../../core/services/setting.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-farmed-type-nut-vals-detalis',
-  templateUrl: './farmed-type-nut-vals-detalis.component.html',
-  styleUrls: ['./farmed-type-nut-vals-detalis.component.scss']
+  selector: "app-farmed-type-nut-vals-detalis",
+  templateUrl: "./farmed-type-nut-vals-detalis.component.html",
+  styleUrls: ["./farmed-type-nut-vals-detalis.component.scss"],
 })
 export class FarmedTypeNutValsDetalisComponent implements OnInit {
-
+  id: any;
   constructor(
     private _CrudRequestsService: CrudRequestsService,
     private _SettingService: SettingService,
@@ -25,8 +25,11 @@ export class FarmedTypeNutValsDetalisComponent implements OnInit {
   }
   detailsData: any = {};
   getUser = (id: any) => {
-    this._CrudRequestsService.get("farmed_type_nut_vals/" + id).subscribe((data: any) => {
-      this.detailsData = data.data;
-    });
+    this._CrudRequestsService
+      .get("farmed_type_nut_vals/" + id)
+      .subscribe((data: any) => {
+        this.detailsData = data.data;
+        this.id = data.data.farmed_type_id;
+      });
   };
 }
